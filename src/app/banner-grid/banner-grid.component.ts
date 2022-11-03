a74032d91c3d9c78bf2de583a15b5717acf871dc
ZeroGick <medinaalzatehectorduvan@gmail.com>
2022-11-03 15:58:10 -0500

    first commit

---
+++ src/app/banner-grid/banner-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-grid',
+  templateUrl: './banner-grid.component.html',
+  styleUrls: ['./banner-grid.component.css']
+})
+export class BannerGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
